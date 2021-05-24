fe69319d3f65ee18fe235262749f32556a535b74
lamisghanayem <ghanayemlamis@gmail.com>
2021-05-24 15:21:50 +0300

    Updated 4 questions

--- js/app.js
+++ js/app.js
@@ -18,10 +18,50 @@ else {
   alert('Your answer should be Yes or No');
 }
 
-let country = prompt ('Do I come from Jordan"?');
+let country = prompt ('Do I come from Jordan"?').toLowerCase();
+if ( country === 'yes' || country === 'y'){
+  alert ('Correct! Let us continue.');
+}
+else if ( country === 'no' || country === 'n')
+{
+  alert ('wrong answer.');
+}
+else {
+  alert('Your answer should be Yes or No');
+}
 
-let degree = prompt ('Do I hold a bachelor degree in physics?');
+let degree = prompt ('Do I hold a bachelor degree in physics?').toLowerCase();
+if ( degree === 'yes' || degree === 'y'){
+  alert ('Correct! Let us continue.');
+}
+else if ( degree === 'no' || degree === 'n')
+{
+  alert ('wrong answer.');
+}
+else {
+  alert('Your answer should be Yes or No');
+}
 
-let experience = prompt ('Do I have work experience in my major?');
 
-let university = prompt ('Did I graduate from the Lebanese International University?');
+let experience = prompt ('Do I have work experience in my major?').toLowerCase();
+if ( experience === 'yes' || experience === 'y'){
+  alert ('Correct! Let us continue.');
+}
+else if ( experience === 'no' || experience === 'n')
+{
+  alert ('wrong answer.');
+}
+else {
+  alert('Your answer should be Yes or No');
+}
+let university = prompt ('Did I graduate from the Lebanese International University?').toLowerCase();
+if ( university === 'yes' || university === 'y'){
+  alert ('Correct! Let us continue.');
+}
+else if ( university === 'no' || university === 'n')
+{
+  alert ('wrong answer.');
+}
+else {
+  alert('Your answer should be Yes or No');
+}
